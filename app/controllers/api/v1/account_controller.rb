471f369b9d04279f2bdfda27f19f832460caa785
sbower <shawn.bower@gmail.com>
2016-02-22 20:03:11 -0500

    additions for ping page, added aggregate function to group by cost center

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -1,33 +1,43 @@
-module API  
+module API
   module V1
     class AccountController < ApplicationController
       before_action :authenticate
-      
+
       def index
         if @current_user.admin?
           @details = BillingDetail.all
-          render json: @details 
-        else
-          render json: 'Bad credentials', status: 401
-        end
-      end
-      
-      def show
-        if @current_user.has_account_access?(params[:id]) or @current_user.admin?
-          if params[:month] && params[:year]
-            @details = BillingDetail.linked_account(params[:id]).by_month(params[:month]).by_year(params[:year])
-          elsif params[:year]
-            @details = BillingDetail.linked_account(params[:id]).by_year(params[:year])
-          else
-            @details = BillingDetail.linked_account(params[:id])
-          end
-        
           render json: @details
         else
           render json: 'Bad credentials', status: 401
         end
       end
-      
+
+      def show
+        if @current_user.has_account_access?(params[:id]) or @current_user.admin?
+          if params[:month] && params[:year]
+            @details = BillingDetail.linked_account(params[:id]).by_month(params[:month]).by_year(params[:year])
+            render json: @details
+          else
+            render json: 'Must supply year and month', status: 501
+          end
+        else
+          render json: 'Bad credentials', status: 401
+        end
+      end
+
+      def aggregate
+        if @current_user.has_account_access?(params[:id]) or @current_user.admin?
+          if params[:month] && params[:year]
+            @details = BillingDetail.linked_account(params[:id]).by_month(params[:month]).by_year(params[:year]).group(:cost_center).sum(:blended_cost)
+            render json: @details
+          else
+            render json: 'Must supply year and month', status: 501
+          end
+        else
+          render json: 'Bad credentials', status: 401
+        end
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  namespace :api do  
+  namespace :api do
     namespace :v1 do
-      resources :account
+      resources :account do
+        member do
+          get 'aggregate'
+        end
+      end
       resources :user
       resources :current_month
+      resources :up_test
     end
   end
 end
