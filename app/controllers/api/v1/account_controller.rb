6b67d6fb629d702e6db71fd95c56c2153b7c1586
Paul Allen <pea1@cornell.edu>
2017-07-14 15:35:25 -0400

    switched to unblended costs

--- app/controllers/api/v1/account_controller.rb
+++ app/controllers/api/v1/account_controller.rb
@@ -7,7 +7,7 @@ module API
         if @current_user.admin?
           if params[:month] && params[:year]
             @details = []
-            @details_array = ActiveRecord::Base.connection.execute("select linked_account_id, kfs_account, cost_center, business_purpose, sum(blended_cost) from billing_detail a, aws_accounts b where a.month = '#{params[:month]}' and a.year = '#{params[:year]}' and a.linked_account_id = b.account_number group by linked_account_id, cost_center, business_purpose, kfs_account")
+            @details_array = ActiveRecord::Base.connection.execute("select linked_account_id, kfs_account, cost_center, business_purpose, sum(unblended_cost) from billing_detail a, aws_accounts b where a.month = '#{params[:month]}' and a.year = '#{params[:year]}' and a.linked_account_id = b.account_number group by linked_account_id, cost_center, business_purpose, kfs_account")
 
             @details_array.each do |detail|
               @details.push({
@@ -43,7 +43,7 @@ module API
       def aggregate
         if @current_user.has_account_access?(params[:id]) or @current_user.admin?
           if params[:month] && params[:year]
-            @details = BillingDetail.linked_account(params[:id]).by_month(params[:month]).by_year(params[:year]).group(:cost_center).sum(:blended_cost)
+            @details = BillingDetail.linked_account(params[:id]).by_month(params[:month]).by_year(params[:year]).group(:cost_center).sum(:unblended_cost)
             render json: @details
           else
             render json: 'Must supply year and month', status: 501
